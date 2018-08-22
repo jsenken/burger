8ac8d54928e6f7cbf3ae9568068f97063d0023c3
Jordan Senken <jordansenken@Jordans-MacBook-Pro.local>
2018-08-21 21:47:33 -0400

    init commit

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        console.log("this fired also");
+    
+        var newBurger = {
+            name: $("#burg").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+    
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new Burger");
+                location.reload();
+            }
+        );
+    });
+});
+
+
+
+// $("#change-eat").on("click", function(event) {
+//     console.log("this fired")
+//     var id = $(this).data("id");
+//     var newBurger = $(this).data("newBurger");
+//     var newEatState = {
+//         eaten : true
+//     };
+
+//     S.ajax("/api/burgers/" + id, {
+//         type: "PUT",
+//         data: newEatState
+//     }).then(
+//         function() {
+//             console.log("changed eat to", newBurger);
+//             location.reload();
+//         }
+//     );
+// });
+
+//needed to undevour something
